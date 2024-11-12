b57bbee137001a1dd095e23b7e9b3db045b39d81
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-11-12 14:47:03 +0100

    update style

--- src/app/body/my-skills/my-skills.component.ts
+++ src/app/body/my-skills/my-skills.component.ts
@@ -10,7 +10,7 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-  iconImgs = ["angular.png", "django.svg","typescript.svg", "javascript.svg", "python.png", "html.svg", "css.svg", "sass.svg","firebase.svg", "git.svg", "scrum.svg", "API.svg", "github.png", "linux.png", "postgresql.png", "postman.svg", "redis-logo.svg", "sql.png"];
-  iconText = ["Angular", "Django", "TypeScript", "JavaScript", "Python","HTML", "CSS", "SASS","Firebase", "git", "Scrum", "API", "GitHub", "Linux","PostgreSQL","Postman", "Redis", "SQL"];
+  iconImgs = ["angular.png", "django.svg","typescript.svg", "javascript.svg", "python.png", "html.svg", "sass.svg", "postman.svg","firebase.svg", "git.svg", "scrum.svg", "API.svg", "github.png", "linux.png", "postgresql.png", "css.svg", "redis-logo.svg", "sql.png"];
+  iconText = ["Angular", "Django", "TypeScript", "JavaScript", "Python","HTML", "SASS", "Postman","Firebase", "git", "Scrum", "API", "GitHub", "Linux","PostgreSQL", "CSS", "Redis", "SQL"];
 
 }
